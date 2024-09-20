7d05a008e940ef14f62e586d7faeb9d8cef33ec0
yann-nintunze-vinci <yann.nintunze@student.vinci.be>
2024-09-20 21:24:53 +0200

    edit 1.2

--- exercices/1.1/basic/app.ts
+++ exercices/1.1/basic/app.ts
@@ -7,17 +7,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/films", filmRouter);
-
 //exercice 1.2
 let getRequestCount = 0;
 
 app.use((req, _res, next) => {
     if (req.method === "GET") {
         getRequestCount++;
-        console.log("GET / : " + getRequestCount);
+        console.log(`GET / : ${getRequestCount}`);
     }
     next();
 });
+
+app.use("/films", filmRouter);
 
 export default app;
--- tutorials/back/api/basic/app.ts
+++ tutorials/back/api/basic/app.ts
@@ -9,6 +9,28 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//Bonus
+const requestCounts: { [key: string]: { [key: string]: number } } = {
+  GET: {},
+  POST: {},
+  PUT: {},
+  DELETE: {}
+};
+
+app.use((req, _res, next) => {
+  const url = req.originalUrl;
+  const method = req.method;
+  if (!requestCounts[method]) {
+    requestCounts[method] = {};
+  }
+  if (!requestCounts[method][url]) {
+    requestCounts[method][url] = 0;
+  }
+  requestCounts[method][url]++;
+  console.log(`${method} ${url} : ${requestCounts[method][url]}`);
+  next();
+});
+
 app.use("/users", usersRouter);
 app.use("/pizzas", pizzaRouter);
 app.use("/drinks", drinkRouter);
@@ -17,46 +39,6 @@ const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
   console.error(err.stack);
   return res.status(500).send("Something broke!");
 };
-
-let getRequestCount = 0;
-let postRequestCount = 0;
-
-app.use((req, _res, next) => {
-  const url = req.originalUrl;
-
-  switch (req.method) {
-    case "GET":
-      getRequestCount++;
-      console.log("GET / : " + getRequestCount);
-      switch (url) {
-        case "/pizzas":
-          console.log();
-          break;
-        case "/drinks":
-          console.log();
-          break;
-        case "/users":
-          console.log();
-          break;
-      }
-      break;
-    case "POST":
-      postRequestCount++;
-      console.log("POST / : " + postRequestCount);
-      switch (url) {
-        case "/pizzas":
-          console.log();
-          break;
-        case "/drinks":
-          console.log();
-          break;
-        case "/users":
-          console.log();
-          break;
-      }
-      next();
-  }
-});
 
 app.use(errorHandler);
 
