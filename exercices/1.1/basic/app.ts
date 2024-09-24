19cc95f97836f1ae323d904710dcc2f1ad4ab6dd
yann-nintunze-vinci <yann.nintunze@student.vinci.be>
2024-09-24 09:51:41 +0200

    add 1.2 + bonus a myMovies

--- exercices/1.1/basic/app.ts
+++ exercices/1.1/basic/app.ts
@@ -7,16 +7,27 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//exercice 1.2
-let getRequestCount = 0;
-
-app.use((req, _res, next) => {
-    if (req.method === "GET") {
-        getRequestCount++;
-        console.log(`GET counter : ${getRequestCount}`);
+//1.2 + Bonus
+const requestCounts: { [key: string]: { [key: string]: number } } = {
+    GET: {},
+    POST: {},
+    PUT: {},
+    DELETE: {}
+  };
+  
+  app.use((req, _res, next) => {
+    const url = req.originalUrl;
+    const method = req.method;
+    if (!requestCounts[method]) {
+      requestCounts[method] = {};
     }
+    if (!requestCounts[method][url]) {
+      requestCounts[method][url] = 0;
+    }
+    requestCounts[method][url]++;
+    console.log(`${method} ${url} : ${requestCounts[method][url]}`);
     next();
-});
+  });
 
 app.use("/films", filmRouter);
 
