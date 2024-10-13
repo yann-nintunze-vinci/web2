cecce60d3f094f0999900d4359c627ab44ad60d4
yann-nintunze-vinci <yann.nintunze@student.vinci.be>
2024-10-13 20:40:42 +0200

    tuto: tsx

--- tutorials/components/src/App.tsx
+++ tutorials/components/src/App.tsx
@@ -1,41 +1,62 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import sound from './assets/sounds/01_All_Day.mp3'
 import logo from "./assets/images/js-logo.png";
-import pizzaBackground from "./assets/images/pizza.jpg";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
   return (
-    <>
-      <header>
-        <h1 className="animate__animated animate__bounce">We love Pizza</h1>
-      </header>
-
-      <main>
-        <p>My HomePage</p>
-        <p>
-          Because we love JS, you can also click on the header to stop / start the
-          music ; )
-        </p>
-
-        <audio id="audioPlayer" controls autoPlay>
-          <source
-            src={sound}
-            type="audio/mpeg"
-          />
-          Your browser does not support the audio element.
-        </audio>
-      </main>
-
-      <footer>
-        <h1 className="animate__animated animate__bounce animate__delay-2s">
-          But we also love JS
-        </h1>
-        <img src={logo} alt="" />
-      </footer>
-    </>
-  )
+    <div className='page'>
+      <Header title='We love Pizza' version={0+1}/>
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+const Header = (props : HeaderProps) => {
+  return (
+    <header>
+      <h1 className="animate__animated animate__bounce">{props.title}</h1>
+      <h4>Version: {props.version}</h4>
+    </header>
+  );
+};
+
+const Main = () => {
+  return (
+    <main>
+      <p>My HomePage</p>
+      <p>
+        Because we love JS, you can also click on the header to stop / start the
+        music ; )
+      </p>
+
+      <audio id="audioPlayer" controls autoPlay>
+        <source
+          src={sound}
+          type="audio/mpeg"
+        />
+        Your browser does not support the audio element.
+      </audio>
+    </main>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer>
+      <h1 className="animate__animated animate__bounce animate__delay-2s">
+        But we also love JS
+      </h1>
+      <img src={logo} alt="" />
+    </footer>
+  );
+};
+
+interface HeaderProps {
+  title: string;
+  version: number;
+}
+
+export default App;
