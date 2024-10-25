ae821c9cb5f0a5c64328a887c8091bbf2eda8662
yann-nintunze-vinci <yann.nintunze@student.vinci.be>
2024-10-25 17:33:28 +0200

    edit: description style

--- exercices/2.6/src/components/Movie/index.tsx
+++ exercices/2.6/src/components/Movie/index.tsx
@@ -8,16 +8,11 @@ interface MovieProps {
 
 const Movie = ({ title, director, description }: MovieProps) => {
     const [descPrinted, setDescPrinted] = useState(false);
-    
-    const descriptionStyle = {
-        color: 'grey',
-        fontStyle: 'italic'
-    };
 
     return (
         <>
             <p onClick={() => setDescPrinted(!descPrinted)}>{title} - Realisateur : {director}</p>
-            {descPrinted && <p style={descriptionStyle}>{description}</p>}
+            {descPrinted && <p style={{color: "grey", fontStyle: "italic"}}>{description}</p>}
         </>
 
     );
