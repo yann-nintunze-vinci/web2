84a36d8976f3598b1296a59a0163fa99856ac963
yann-nintunze-vinci <yann.nintunze@student.vinci.be>
2024-11-22 21:24:11 +0100

    new: ex2.13

---
+++ exercices/2.13-14/src/App.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+interface Joke {
+  category: string;
+  joke: string;
+}
+
+function App() {
+  const [joke, setJoke] = useState<Joke | null>(null);
+
+  const fetchJoke = () => {
+    fetch("https://v2.jokeapi.dev/joke/Any?type=single")
+      .then((response) => {
+        if (!response.ok)
+          throw new Error(
+            `fetch error : ${response.status} : ${response.statusText}`
+          );
+        return response.json();
+      })
+      .then((joke: Joke) => {
+        setJoke(joke);
+      })
+      .catch((err) => {
+        console.error("HomePage::error: ", err);
+      });
+  }
+
+  useEffect(() => {
+    fetchJoke();
+  }, []);
+
+  return(
+    <div>
+      {joke ? (
+        <div>
+          <p>{joke.category}</p>
+          <p>{joke.joke}</p>
+        </div>
+      ) : (
+        <p>null</p>
+      )}
+      <button onClick={fetchJoke}>Change Joke</button>
+    </div>
+  );
+}
+
+export default App;
