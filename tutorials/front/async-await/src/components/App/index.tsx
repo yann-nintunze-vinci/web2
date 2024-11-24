17df46339f730fa6bc574c1dd51b17a2e6a31c45
yann-nintunze-vinci <yann.nintunze@student.vinci.be>
2024-11-24 18:21:27 +0100

    tuto: async-await

--- tutorials/front/async-await/src/components/App/index.tsx
+++ tutorials/front/async-await/src/components/App/index.tsx
@@ -40,6 +40,15 @@ const App = () => {
 
   const fetchPizzas = async () => {
     try {
+      const pizzas = await getAllPizzas();
+      setPizzas(pizzas);
+    } catch (err) {
+      console.error("HomePage::error: ", err);
+    }
+  };
+
+  async function getAllPizzas() {
+    try {
       const response = await fetch("http://localhost:3000/pizzas");
 
       if (!response.ok)
@@ -48,15 +57,36 @@ const App = () => {
         );
 
       const pizzas = await response.json();
-      setPizzas(pizzas);
+      return pizzas;
     } catch (err) {
-      console.error("HomePage::error: ", err);
+      console.error("getAllPizzas::error: ", err);
+      throw err;
     }
-  };
+  }
 
-  const addPizza = (newPizza: NewPizza) => {
-    const pizzaAdded = { ...newPizza, id: nextPizzaId(pizzas) };
-    setPizzas([...pizzas, pizzaAdded]);
+  const addPizza = async (newPizza: NewPizza) => {
+    try {
+      const options = {
+        method: "POST",
+        body: JSON.stringify(newPizza),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const response = await fetch("http://localhost:3000/pizzas", options);
+
+      if (!response.ok)
+        throw new Error(
+          `fetch error : ${response.status} : ${response.statusText}`
+        );
+
+      const createdPizza = await response.json();
+
+      setPizzas([...pizzas, createdPizza]);
+    } catch (err) {
+      console.error("AddPizzaPage::error: ", err);
+    }
   };
 
   const handleHeaderClick = () => {
@@ -90,11 +120,6 @@ const App = () => {
       <Footer />
     </div>
   );
-};
-
-const nextPizzaId = (pizzas: Pizza[]) => {
-  const ids = pizzas.map((pizza) => pizza.id);
-  return Math.max(...ids) + 1;
 };
 
 export default App;
