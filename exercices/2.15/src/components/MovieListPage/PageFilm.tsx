2397eba6a09399c2319fbf2f085b309bb0d71279
yann-nintunze-vinci <yann.nintunze@student.vinci.be>
2024-11-24 21:49:51 +0100

    edit: ex2.15

--- exercices/2.15/src/components/MovieListPage/PageFilm.tsx
+++ exercices/2.15/src/components/MovieListPage/PageFilm.tsx
@@ -1,4 +1,5 @@
 import { Film } from "../../types";
+import { useState } from "react";
 
 interface FilmProps {
   films: Film[];
@@ -7,12 +8,35 @@ interface FilmProps {
 const PageFilm = ({ films }: FilmProps) => {
   return (
     <ul>
-      {films.map((films, index) => (
-        <li key={index}>
-          {films.title} - {films.director} - {films.duration} minutes
-        </li>
+      {films.map((film, index) => (
+        <FilmList film={film} index={index} />
       ))}
     </ul>
+  );
+};
+
+interface FilmListProps {
+  film: Film;
+  index: number;
+}
+
+const FilmList = ({ film, index }: FilmListProps) => {
+  const [descPrinted, setDescPrinted] = useState(false);
+
+  return (
+    <li
+      key={index}
+      onClick={() => setDescPrinted(!descPrinted)}
+      className="listFilmPage"
+    >
+      {film.title} - Realisateur : {film.director}
+      {descPrinted && (
+        <span style={{ color: "grey", fontStyle: "italic" }}>
+          <br />
+          {film.duration} minutes
+        </span>
+      )}
+    </li>
   );
 };
 
